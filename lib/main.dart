eb3e37f228d71fd3cec7559c537fea64ba0eb80a
Yangzhedi <uiryzd@163.com>
2019-05-16 19:19:26 +0800

    feat(fonts): add serifSC font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+
+const NotoSerifSCTextStyle = const TextStyle(
+  fontFamily: 'NotoSerifSC',
+);
 
 void main() => runApp(new MyApp());
 
@@ -10,6 +16,10 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
+      //注册路由表
+      routes:{
+        "new_page": (context) => NewRoute(),
+      },
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -47,6 +57,12 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             new Text(
+              "您已经点击了：(思源宋体)",
+              style: NotoSerifSCTextStyle,
+            ),new Text(
+              "您已经点击了：(默认)",
+            ),
+            new Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
@@ -61,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   }));
               },
             ),
+            RandomWordsWidget(),
           ],
         ),
       ),
@@ -81,8 +98,23 @@ class NewRoute extends StatelessWidget {
         title: Text("New route"),
       ),
       body: Center(
-        child: Text("This is new route"),
+        child: Text(
+          "This is new route, 你好",
+          style: NotoSerifSCTextStyle,
+        ),
       ),
+    );
+  }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
     );
   }
 }
